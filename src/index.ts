e4903bc7a496b422c9e4dc7d020463e2d1746aa5
ryu <alessiasophia23@gmail.com>
2024-09-10 23:34:04 -0500

    Separacion de logica(CRUD) en funciones

--- src/index.ts
+++ src/index.ts
@@ -1,201 +1,54 @@
 import http from "node:http";
-import fs from "node:fs/promises";
-import { randomInt } from "node:crypto";
-import { query } from "./models/db";
+import {
+  postUsers,
+  getUsers,
+  updateUsers,
+  deleteUsers,
+  sendJsonResponse,
+} from "./utils";
 
-//Creando servidor HTTP
 const server = http.createServer(async (request, response) => {
   const { method, url } = request;
   let body = "";
 
   if (url === "/") {
-    response.setHeader("Content-Type", "application/json; charset=utf-8");
-    response.end(" Bienvenido a mi Api RESfull");
+    response.setHeader("Content-Type", "text/plain; charset=utf-8");
+    response.end("Bienvenido a mi Api RESfull");
   } else if (url === "/users") {
     switch (method) {
-      //Mostrar la lista de usuarios
       case "GET":
-        try {
-          /* const Content = await fs.readFile("./data.json", {
-            encoding: "utf8",
-          });
-          const data = JSON.parse(Content); */
-          const data = await query("SELECT * FROM users ORDER BY id");
-          response.writeHead(200, { "Content-Type": "application/json" });
-          response.end(JSON.stringify({ ok: true, data: data.rows }));
-        } catch (error) {
-          response.writeHead(500, { "Content-Type": "application/json" });
-          response.end(
-            JSON.stringify({ error: true, message: "Server Error" })
-          );
-        }
+        getUsers(response);
         break;
-      //Agregar un nuevo usuario
+
       case "POST":
-        let body = "";
-
-        request.on("data", (chunk) => {
-          body += chunk.toString();
-        });
-
-        request.on("end", async () => {
-          try {
-            //datos del nuevo usuario
-            const { name, email, role, rate } = JSON.parse(body);
-
-            //Se agrea el usuario nuevo a la lista de usuarios
-            /* const data = await fs.readFile("./data.json", { encoding: "utf8" }); */
-            /*  const users = JSON.parse(data);
-            users.items.push(newUser);
-            await fs.writeFile("./data.json", JSON.stringify(users, null, 2)); */
-            const result = await query(
-              "INSERT INTO users (name,email,role,rate) VALUES ($1, $2, $3, $4) RETURNING *",
-              [name, email, role, rate]
-            );
-
-            response.writeHead(201, { "Content-Type": "application/json" });
-            response.end(JSON.stringify({ ok: true, data: result.rows[0] }));
-          } catch (error) {
-            response.writeHead(400, { "Content-Type": "application/json" });
-            response.end({ error: true, message: "Invalid data" });
-          }
-        });
+        postUsers(request, response);
         break;
     }
   } else if (url?.match(/\/users\/\d+/)) {
     const id = parseInt(url.split("/")[2]);
-    /*  const data = await fs.readFile("./data.json", { encoding: "utf-8" });
-    let users = JSON.parse(data);
-    const dataUser = users.items.find((user: user) => user.id === id); */
-    const dataUser = await query("SELECT *   FROM users WHERE id = $1", [id]);
 
     switch (method) {
       case "GET":
-        try {
-          //Se verifica que exista el usuario y se muestra
-          if (dataUser.rows) {
-            response.writeHead(200, { "Content-Type": "application/json" });
-            response.end(JSON.stringify({ ok: true, data: dataUser.rows }));
-            //Si no existe se muestra un not found
-          } else {
-            response.writeHead(404, { "Content-Type": "application/json" });
-            response.end(
-              JSON.stringify({ error: true, message: "User not found" })
-            );
-          }
-        } catch (error) {
-          response.writeHead(500, { "Content-Type": "application/json" });
-          response.end(
-            JSON.stringify({ error: true, message: "Server Error" })
-          );
-        }
+        getUsers(response, id);
         break;
+
       case "PATCH":
-        request.on("data", (chunk) => {
-          body += chunk.toString();
-        });
-
-        request.on("end", async () => {
-          try {
-            if (dataUser.rows) {
-              const userUpdate = { ...dataUser.rows[0], ...JSON.parse(body) };
-              const { name, email, role, rate } = userUpdate;
-              const result = await query(
-                "UPDATE users SET name = $1, email = $2, role = $3, rate = $4 WHERE id = $5  RETURNING *",
-                [name, email, role, rate, id]
-              );
-              /* users.items = users.items.map((user: user) =>
-                user.id === dataUser.id
-                  ? { id: dataUser.id, ...userUpdate }
-                  : user
-              );
-              await fs.writeFile("./data.json", JSON.stringify(users, null, 2)); */
-              response.writeHead(200, { "Content-Type": "application/json" });
-              response.end(
-                JSON.stringify({
-                  ok: true,
-                  data: result.rows[0],
-                })
-              );
-            } else {
-              response.writeHead(404, { "Content-Type": "application/json" });
-              response.end(
-                JSON.stringify({ error: true, message: "User not found" })
-              );
-            }
-          } catch (error) {
-            response.writeHead(500, { "Content-Type": "application/json" });
-            response.end(
-              JSON.stringify({ error: true, message: "Server Error" })
-            );
-          }
-        });
+        updateUsers(request, response, id);
         break;
+
       case "PUT":
-        request.on("data", (chunk) => {
-          body += chunk.toString();
-        });
-
-        request.on("end", async () => {
-          try {
-            if (dataUser.rows) {
-              const userUpdate = { ...dataUser.rows[0], ...JSON.parse(body) };
-              const { name, email, role, rate } = userUpdate;
-              const result = await query(
-                "UPDATE users SET name = $1, email = $2, role = $3, rate = $4 WHERE id = $5  RETURNING *",
-                [name, email, role, rate, id]
-              );
-              response.writeHead(200, { "Content-Type": "application/json" });
-              response.end(
-                JSON.stringify({
-                  ok: true,
-                  data: result.rows[0],
-                })
-              );
-            } else {
-              response.writeHead(404, { "Content-Type": "application/json" });
-              response.end(
-                JSON.stringify({ error: true, message: "User not found" })
-              );
-            }
-          } catch (error) {
-            response.writeHead(500, { "Content-Type": "application/json" });
-            response.end(
-              JSON.stringify({ error: true, message: "Server Error" })
-            );
-          }
-        });
+        updateUsers(request, response, id);
         break;
+
       case "DELETE":
-        try {
-          if (dataUser.rows) {
-            const result = await query(
-              "DELETE FROM users WHERE id = $1 RETURNING *",
-              [id]
-            );
-            /* users.items = users.items.filter(
-              (user: user) => user.id !== dataUser.id
-            );
-            await fs.writeFile("./data.json", JSON.stringify(users, null, 2)); */
-            response.writeHead(200, { "Content-Type": "application/json" });
-            response.end(JSON.stringify({ ok: true, data: result.rows[0] }));
-          } else {
-            response.writeHead(404, { "Content-Type": "application/json" });
-            response.end(
-              JSON.stringify({ error: true, message: "User not found" })
-            );
-          }
-        } catch (error) {
-          response.writeHead(500, { "Content-Type": "application/json" });
-          response.end(
-            JSON.stringify({ error: true, message: "Server Error" })
-          );
-        }
+        deleteUsers(response, id);
         break;
     }
   } else {
-    response.writeHead(404, { "Content-Type": "application/json" });
-    response.end(JSON.stringify({ error: true, message: "Route not found" }));
+    sendJsonResponse(response, 404, {
+      error: true,
+      message: "Route not found",
+    });
   }
 });
 
---
+++ src/utils.ts
@@ -0,0 +1,125 @@
+import { ServerResponse, IncomingMessage } from "node:http";
+import { query } from "./models/db";
+
+export const sendJsonResponse = (
+  res: ServerResponse,
+  statusCode: number,
+  data: object | string
+) => {
+  res.writeHead(statusCode, {
+    "Content-Type": "application/json; charset=utf-8",
+  });
+  res.end(JSON.stringify(data));
+};
+
+export function postUsers(request: IncomingMessage, response: ServerResponse) {
+  let body = "";
+
+  request.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+
+  request.on("end", async () => {
+    try {
+      const { name, email, role, rate } = JSON.parse(body);
+      const result = await query(
+        "INSERT INTO users (name,email,role,rate) VALUES ($1, $2, $3, $4) RETURNING *",
+        [name, email, role, rate]
+      );
+      sendJsonResponse(response, 201, { ok: true, data: result.rows[0] });
+    } catch (error) {
+      sendJsonResponse(response, 400, {
+        error: true,
+        message: "Invalid data",
+      });
+    }
+  });
+}
+
+export async function getUsers(response: ServerResponse, id?: number) {
+  try {
+    const sentencia = id
+      ? `SELECT * FROM users WHERE id = $1`
+      : "SELECT * FROM users ORDER BY id";
+    const dataUser = await query(sentencia, id ? [id] : []);
+
+    if (dataUser.rows.length > 0) {
+      sendJsonResponse(response, 200, { ok: true, data: dataUser.rows });
+    } else {
+      sendJsonResponse(response, 404, {
+        error: true,
+        message: "User Not Found",
+      });
+    }
+  } catch (error) {
+    sendJsonResponse(response, 500, {
+      error: true,
+      message: "Server Error",
+    });
+  }
+}
+
+export async function updateUsers(
+  request: IncomingMessage,
+  response: ServerResponse,
+  id: number
+) {
+  let body = "";
+
+  request.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+
+  request.on("end", async () => {
+    try {
+      const dataUser = await query(`SELECT * FROM users WHERE id = $1`, [id]);
+
+      if (dataUser.rows.length > 0) {
+        const userUpdate = { ...dataUser.rows[0], ...JSON.parse(body) };
+        const { name, email, role, rate } = userUpdate;
+        const result = await query(
+          "UPDATE users SET name = $1, email = $2, role = $3, rate = $4 WHERE id = $5  RETURNING *",
+          [name, email, role, rate, id]
+        );
+        sendJsonResponse(response, 200, {
+          ok: true,
+          data: result.rows[0],
+        });
+      } else {
+        sendJsonResponse(response, 404, {
+          error: true,
+          message: "User Not Found",
+        });
+      }
+    } catch (error) {
+      sendJsonResponse(response, 500, {
+        error: true,
+        message: "Server Error",
+      });
+    }
+  });
+}
+
+export async function deleteUsers(response: ServerResponse, id: number) {
+  try {
+    const dataUser = await query("SELECT * FROM users WHERE id = $1", [id]);
+
+    if (dataUser.rows.length > 0) {
+      const result = await query("DELETE FROM users WHERE id = $1", [id]);
+      sendJsonResponse(response, 200, {
+        ok: true,
+        message: "User deleted successfully",
+      });
+    } else {
+      sendJsonResponse(response, 404, {
+        error: true,
+        message: "User Not Found",
+      });
+    }
+  } catch (error) {
+    sendJsonResponse(response, 500, {
+      error: true,
+      message: "Server Error",
+    });
+  }
+}
